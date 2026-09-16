d884e2ce0e389170942e711a5c8bb0d058ec8f25
cee250 <2.06686419e+08+cee250@users.noreply.github.com>
2026-09-16 08:35:47 +0000

    feat(phase-21): notify customers when orders are created

--- server/src/orders/orders.module.ts
+++ server/src/orders/orders.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 
-@Module({ imports: [AuthModule], controllers: [OrdersController], providers: [OrdersService] })
+@Module({ imports: [AuthModule, NotificationsModule], controllers: [OrdersController], providers: [OrdersService] })
 export class OrdersModule {}
